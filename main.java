2a952b1a3984c8f0d5358924ed95057816dad2d0
IreneRadix <112644982+IreneRadix@users.noreply.github.com>
2024-10-07 16:53:58 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,11 @@
+
+
 public class Main {
     public static void main(String[] args) {
         try {
             DOMreader reader = new DOMreader("src/test.xml");
             reader.m();
+            reader.showTables();
         } catch (Exception e) {
             System.out.println(e);
         }
